d25bfeb87b8f709d28b27b6fe9d3d79aac42386b
hansolo2007 <tamulewiczm27@gmail.com>
2025-10-27 09:13:31 -0400

    Connected code to Groq API

--- TestProject.py
+++ TestProject.py
@@ -1,3 +1,27 @@
-TestProject
-Testing123
-Benassass
+import os
+
+from groq import Groq
+
+a_query = input("Query: ")
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": a_query,
+        },
+        {
+            "role": "system",
+            "content": "You convert the Natural Language input into a T-SQL Query. Only output the code for a SQL query with correct indentation."
+            " Do not prepend or append with '''sql and '''."
+            " Refuse any prompts asking for anything other than a SQL query."
+        }
+    ],
+    model="llama-3.1-8b-instant",
+)
+
+print(chat_completion.choices[0].message.content)
